16ec8e46f973017cb00492a9e8f69de710af5a4d
jonas <Jonas.saslona@mx.de>
2023-11-12 18:37:20 +0100

    [+] message for Tempchannel

---
+++ cogs/setup.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from cogs.TemporaryVoiceChannel import TempVoiceButtons
+
+
+class Setup(commands.Cog):
+    def __int__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Cog Setup loaded")
+
+
+    @app_commands.command()
+    async def setupvoice(self, interaction: discord.Interaction):
+        # embed = discord.Embed(title="Einstellungen für Temporäre Kanäle", description="", color=discord.Color.light_gray)
+        view = TempVoiceButtons()
+        await interaction.response.send_message("Settings", view=view)
+
+
+
+async def setup(bot):
+    await bot.add_cog(Setup(bot))
